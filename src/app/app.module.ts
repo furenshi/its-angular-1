9e4992fa1902482e9b0652f18687d7f833713f51
Francesco Abrate <francescoabrate21@gmail.com>
2022-11-30 12:01:20 +0100

    folders  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { BlockUIModule } from 'ng-block-ui';
-import { BlockUIHttpModule } from 'ng-block-ui/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DrinkComponent } from './pages/drink/drink.component';
-import { SearchComponent } from './pages/search/search.component';
-import { CardDrinkComponent } from './shared/card-drink/card.drink.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { OrdiniComponent } from './pages/ordini/ordini.component';
+import { CartItemComponent } from './shared/cartItem/cartItem.component';
+
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockUIHttpModule } from 'ng-block-ui/http';
 
 @NgModule({
   declarations: [
@@ -21,19 +21,20 @@ import { CardDrinkComponent } from './shared/card-drink/card.drink.component';
     LoginComponent,
     HomeComponent,
     DrinkComponent,
-    SearchComponent,
-    CardDrinkComponent,
+    IngredientsComponent,
+    OrdiniComponent,
+    CartItemComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
+    HttpClientModule,
     NgbModule,
     FormsModule,
-    BlockUIModule.forRoot(),
     BlockUIHttpModule.forRoot(),
+    BlockUIModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ApiService } from 'src/app/_service/api.service';
 
 @Component({
@@ -6,26 +6,164 @@ import { ApiService } from 'src/app/_service/api.service';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  drinks:any[] = [];
-  letters =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  firstLetter = '';
+  collapsed = true;
+  isChecked: boolean = false;
+  typeOfSearch: string = '';
+  searchNameIng: string = '';
+  drinks: any[] = [];
+  letters: any[] = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) { }
 
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
   ngOnInit(): void {
-    this.changeFirstLetter('A');
+    this.apiService
+      .searchCocktailByFirstLetter('a')
+      .subscribe((response: any) => {
+        console.log(response);
+        this.drinks = response.drinks;
+        if (!this.drinks) {
+          this.drinks = [];
+        }
+
+        this.drinks.sort((a: any, b: any) => {
+          const nameA = a.strDrink.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.strDrink.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          // names must be equal
+          return 0;
+        });
+      });
   }
 
-  changeFirstLetter(letter: string) {
-    this.firstLetter = letter;
-    this.apiService.searchCocktailByFirstLetter(this.firstLetter)
-    .subscribe( (response: any) => {
+  searchByLetter = (letter: string) => {
+    this.apiService
+      .searchCocktailByFirstLetter(letter)
+      .subscribe((response: any) => {
+        console.log(response);
+        this.drinks = response.drinks;
+        if (!this.drinks) {
+          this.drinks = [];
+        }
+
+        this.drinks.sort((a: any, b: any) => {
+          const nameA = a.strDrink.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.strDrink.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          // names must be equal
+          return 0;
+        });
+      });
+  };
+
+  searchByName = () => {
+    this.apiService
+      .searchCocktailByName(this.searchNameIng)
+      .subscribe((response: any) => {
+        console.log(response);
+        this.isChecked = false;
+        if (!response) {
+          this.drinks = [];
+        } else {
+          this.drinks = response.drinks;
+        }
+
+        if (!this.drinks) {
+          this.drinks = [];
+          this.isChecked = false;
+        }
+
+        this.drinks.sort((a: any, b: any) => {
+          const nameA = a.strDrink.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.strDrink.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+
+          // names must be equal
+          return 0;
+        });
+      });
+  };
+
+  searchByIngredients = () => {
+    this.apiService
+      .searchCocktailByIngredient(this.searchNameIng)
+      .subscribe((response: any) => {
+        console.log(response);
+        this.isChecked = false;
+        if (!response) {
+          this.drinks = [];
+        } else {
+          this.drinks = response.drinks;
+        }
+
+        if (!this.drinks) {
+          this.drinks = [];
+          this.isChecked = false;
+        }
+      });
+  };
+
+  search = () => {
+    if (this.typeOfSearch === 'Cocktail') {
+      this.isChecked = true;
+      return this.searchByName();
+    } else if (this.typeOfSearch === 'Ingredient') {
+      this.isChecked = true;
+      return this.searchByIngredients();
+    }
+  };
+
+  randomCocktail = () => {
+    this.apiService.searchRandomCocktail().subscribe((response: any) => {
+      console.log(response);
       this.drinks = response.drinks;
-    })
-  }
-
-  onCardSelectChange(drink: any, $event: boolean) {
-    console.log("drink", drink, "selezionato?", $event);
-    drink.selected = $event;
-  }
+      if (!this.drinks) {
+        this.drinks = [];
+      }
+    });
+  };
 }
